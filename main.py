741d6de4e928ee4056723610fdb59c425e1e868f
Youssef <21P0094@eng.asu.edu.eg>
2025-05-10 00:30:06 +0300

    .

--- main.py
+++ main.py
@@ -48,18 +48,18 @@ def main():
         from master_node import master_process
         master_process()
 
-    #elif rank == 1:  # server node
-    #    node_type = "Server"
-    #    logging.info(f"Process {rank} starting as Server")
-    #    from server import start_server
-    #    start_server()
+    elif rank == 1:  # server node
+        node_type = "Server"
+        logging.info(f"Process {rank} starting as Server")
+        from server import start_server
+        start_server()
     
-    #elif rank == size - 1:  # Last rank is the indexer
-    #    node_type = "Indexer"
-    #    logging.info(f"Process {rank} starting as Indexer node")
-    #    # Import indexer module and run indexer process
-    #    from indexer_main import indexer_node
-    #    indexer_node()
+    elif rank == size - 1:  # Last rank is the indexer
+        node_type = "Indexer"
+        logging.info(f"Process {rank} starting as Indexer node")
+        # Import indexer module and run indexer process
+        from indexer_main import indexer_node
+        indexer_node()
         
     else:  # Other ranks are crawlers
         node_type = "Crawler"
